1faffcf2b0f669c2952ed2f7c9b07ad6b5702835
sosei333 <soseiyanen@gmail.com>
2024-11-14 09:41:37 +0900

    add back2

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -1,8 +1,12 @@
 -- backend/init.sql
+ALTER USER 'sosei'@'%' IDENTIFIED WITH mysql_native_password BY 'Niko#124';
+
 CREATE DATABASE IF NOT EXISTS hackathon;
 USE hackathon;
 
 CREATE TABLE IF NOT EXISTS users (
     id VARCHAR(255) PRIMARY KEY,
-    email VARCHAR(255) NOT NULL
+    user_name VARCHAR(255) NOT NULL
 );
+
+FLUSH PRIVILEGES;
