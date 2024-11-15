25a0dec74eae2c305c106cfe4e71510f1189609e
sosei333 <soseiyanen@gmail.com>
2024-11-15 17:41:31 +0900

    deploy

--- init/init.sql
+++ init/init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS mydatabase;
 USE mydatabase;
 
 CREATE TABLE IF NOT EXISTS users (
-    id VARCHAR(255) PRIMARY KEY,
+    user_id VARCHAR(255) PRIMARY KEY,
     user_name VARCHAR(255) NOT NULL
 );
 
