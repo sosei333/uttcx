10b13869ccaca6722b0329fe880096ff11170472
sosei333 <soseiyanen@gmail.com>
2024-12-07 16:17:11 +0900

    modify

--- appx/src/firebase.ts
+++ appx/src/firebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -15,4 +14,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const storage = getStorage(app);
